50ef47e1c6f5a721f9a5406f3f693fafa0608255
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2021-08-07 12:32:28 +0300

    fix: refactored type checking in buffer

--- tools/exceptions/exceptions.hpp
+++ tools/exceptions/exceptions.hpp
@@ -1,6 +1,8 @@
 #pragma once 
 
-#define buffer_with_coords_assertion_error "buffer without coords can't be used here"
-#define buffer_only_coords_assertion_error "not coords only buffer can't be used"
-#define buffer_with_symbol_type_assertion_error "buffer without symbol type can't be used"
-#define search_buffer_assertion_error 
+// #define buffer_with_coords_assertion_error "buffer without coords can't be used here"
+// #define buffer_only_coords_assertion_error "not coords only buffer can't be used"
+// #define buffer_with_symbol_type_assertion_error "buffer without symbol type can't be used"
+// #define search_buffer_assertion_error 
+
+#define buffer_assertion_error "buffer with such type is not allowed"
