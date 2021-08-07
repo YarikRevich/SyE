ab9aad2f2862d94757c86d3e0aae063b5c53b354
yaroslav <yariksvitlitskiy2@gmail.com>
2021-08-07 15:31:29 +0300

    fix: fixed refactor breaks

--- internal/keys/keys.hpp
+++ internal/keys/keys.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
+#include <tuple>
+#include <vector>
+
 #ifdef __APPLE__
 #define K_BACKSPACE 127
-#elif defined(linux)
+#elif defined(__linux__)
 #define K_BACKSPACE 263
 #endif
 
 #define K_COLON 58
 #define K_ENTER 10
 
-#include <tuple>
-#include <vector>
 
 std::tuple<std::vector<char>, bool> ctrl(int ch);
