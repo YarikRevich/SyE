01390750705220d9d72dd8766cd08b3219b0da05
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2021-08-31 17:51:53 +0300

    fix: fixed github test workflow

---
+++ tests/CMakeFiles/test1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../cmd/test1"
+  "../cmd/test1.pdb"
+  "CMakeFiles/test1.dir/buf_test.cpp.o"
+  "CMakeFiles/test1.dir/buf_test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Volumes/Files/c/editor/tests
+# Build directory: /Volumes/Files/c/editor/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(buf_test "/Volumes/Files/c/editor/cmd/test1")
+set_tests_properties(buf_test PROPERTIES  _BACKTRACE_TRIPLES "/Volumes/Files/c/editor/tests/CMakeLists.txt;13;add_test;/Volumes/Files/c/editor/tests/CMakeLists.txt;0;")
